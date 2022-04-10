95e760c5fc0e906972a40264816130bed75e1677
Brainybrian316 <brainybrian316@gmail.com>
2022-04-10 19:14:56 -0400

    testing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-console.log(profileDataArgs);
+//  ! everything up to this point was learning purposes. 
+// console.log(profileDataArgs);
 
 //  * this is the same as the forEach() method below
 // const printProfileData = profileDataArr => {
@@ -9,18 +10,21 @@ console.log(profileDataArgs);
 //     }
 // };
 
-// ! for each  method meant specifically for arrays
+// * for each  method meant specifically for arrays
 // const printProfileData = profileDataArr => {
 //     profileDataArr.forEach((profileItem) => {
 //         console.log(profileItem);
 //     });
 // };
 
-//  function with one return action can be written like this. We are using the foreach method
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// //  function with one return action can be written like this. We are using the foreach method
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
 
+//  TODO: New learning point to create an html file without the DOM
 
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
 
-printProfileData(profileDataArgs);
+console.log(generatePage('Jane', 'janehub'));
