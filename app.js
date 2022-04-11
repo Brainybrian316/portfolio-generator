00f6e367243628371970236c9e726c8df2a79f80
Brainybrian316 <brainybrian316@gmail.com>
2022-04-10 21:11:39 -0400

    generate the html file

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const fs = require('fs');
+// const profileDataArgs = process.argv.slice(2, process.argv.length)
+// const [inputName, github] = profileDataArgs;
+
+
+// * extract arguments and store them into distinct variables
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// * ES6 featured called assignment destructuring
+
+
 
 //  ! everything up to this point was learning purposes. 
 // console.log(profileDataArgs);
@@ -25,6 +35,49 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 //  TODO: New learning point to create an html file without the DOM
 
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
 
-console.log(generatePage('Jane', 'janehub'));
+// const generatePage = (userName, githubName) => {
+//     return `
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//         <title>Document</title>
+//     </head>
+
+//     <body>
+//     <h1>${inputName}</h1>
+//     <h2><a href="https://github.com/${github}">Github</a></h2>
+//     </body>
+//     </html>
+//     `;
+// };
+
+// fs.writeFile('index.html', generatePage(inputName, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output');
+// });
+
+// ! refactoring code with new knowledge 
+
+// variable to load the html file
+const fs = require('fs');
+// require statement to include the generatePage function in this file. 
+const generatePage = require('./src/page-template');
+//  variable to process the node arguments
+const profileDataArgs = process.argv.slice(2);
+//  variable for the name and github values in an array/object to be passed into the generatePage function
+const [inputName, github] = profileDataArgs;
+// variable to store the generatePage function output
+const pageHTML = generatePage(inputName, github);
+
+// method to write the html file to the file system
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output');
+});
