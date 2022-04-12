a6eb7cd08f71fa4ee0fa3dfa47bd3ad15575cc04
Brainybrian316 <brainybrian316@gmail.com>
2022-04-11 22:39:24 -0400

    working with inquirer

--- app.js
+++ app.js
@@ -98,9 +98,24 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide a short description about yourself:'
+            message: 'Provide a short description about yourself:',
+            when: ({
+                confirmAbout
+            }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
